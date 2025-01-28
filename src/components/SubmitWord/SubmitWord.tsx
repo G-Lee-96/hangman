49850e9603b968cee35c7a751da6338e7face899
Gene Lee <gene.lee@franklinwireless.com>
2025-01-28 01:48:22 -0500

    Addition: Submit word input field

---
+++ src/components/SubmitWord/SubmitWord.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./SubmitWord.css";
+type SubmitWordProps = {
+  emitWord: (word: string) => void;
+};
+const SubmitWord: React.FC<SubmitWordProps> = ({ emitWord }): JSX.Element => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredValue = event.target.value.replace(/[0-9]/g, "");
+    setInputValue(filteredValue);
+  };
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    emitWord(inputValue);
+    setInputValue("");
+  };
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          onChange={handleChange}
+          id="wordInput"
+          name="word"
+          value={inputValue}
+          placeholder="Enter a word"
+        ></input>
+        <button type="submit">Update Word</button>
+      </form>
+      <small className="caption">Cannot enter numbers</small>
+    </div>
+  );
+};
+export default SubmitWord;
